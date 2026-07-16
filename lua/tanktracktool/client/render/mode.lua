6e2132dd3e565b7f0fdd7e570fab594d34586a9e
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-07-17 00:10:49 +0300

    Add dormancy check to PostDrawOpaqueRenderables too
    Rendering still can be called if the entity is dormant, which is also a waste of resources
    
    This simple change should improve this

--- lua/tanktracktool/client/render/mode.lua
+++ lua/tanktracktool/client/render/mode.lua
@@ -24,20 +24,28 @@ local draws, emptyCSENT = tanktracktool.render.draws, tanktracktool.render.empty
 local eyepos, eyedir = Vector(), Vector()
 local flashlightMODE
 
-hook.Add( "PostDrawOpaqueRenderables", "tanktracktoolRenderDraw", function(bDrawingDepth, isDrawSkybox, isDraw3DSkybox)
+hook.Add( "PostDrawOpaqueRenderables", "tanktracktoolRenderDraw", function( bDrawingDepth, isDrawSkybox, isDraw3DSkybox )
     if bDrawingDepth or isDrawSkybox then return end
+
     flashlightMODE = LocalPlayer():FlashlightIsOn() --or #ents.FindByClass "*projectedtexture*" ~= 0
     eyepos = EyePos()
     eyedir = EyeVector()
-    if FrameTime() == 0 or not next( draws ) then return end
+
+    if not next( draws ) then
+        return
+    end
+
     if not IsValid( emptyCSENT ) then
         tanktracktool.render.empty = ClientsideModel( "models/props_c17/oildrum001_explosive.mdl" )
         tanktracktool.render.empty:SetNoDraw( true )
         emptyCSENT = tanktracktool.render.empty
         return
     end
+
     for controller, mode in pairs( draws ) do
-        if IsValid( controller ) then mode:draw( controller ) end
+        if IsValid( controller ) and not controller:IsDormant() then
+            mode:draw( controller )
+        end
     end
 end )
 
